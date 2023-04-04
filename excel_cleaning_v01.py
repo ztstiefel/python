e82786284f0493cb2a33489664725936963b8eb3
Zach S <41418360+ztstiefel@users.noreply.github.com>
2023-04-03 19:20:27 -0500

    Update excel_cleaning_v01.py
    added two new functions to continue to clean data and make it ready for analysis.

--- excel_cleaning_v01.py
+++ excel_cleaning_v01.py
@@ -1,8 +1,8 @@
 import pandas as pd
 import os
 
-dir = '<user_dir>' # directory with input files
-new_dir = '<user_dir>' # directory where output files should go
+dir = 'C:\\Users\\ztsti\\Downloads\\grades' # directory with input files
+new_dir = 'C:\\Users\\ztsti\\Downloads\\grades\\cleaned' # directory where output files should go
 
 def del_rows_and_cols():
     for file in os.listdir(dir):
@@ -18,3 +18,35 @@ def del_rows_and_cols():
             df.to_excel(os.path.join(new_dir,os.path.basename(file)),  index=False, header=True) # write out modified file
 
 del_rows_and_cols()
+
+def clean_col_headers():
+    for file in os.listdir(dir):
+        f = os.path.join(dir, file)
+        if os.path.isfile(f):
+            df = pd.read_excel(file) # open excel file
+            df.columns = df.columns.str.replace('\n', '-',) # replace newline chars in column titles with dash char
+            df.columns = df.columns.str.replace(" ","_") # replace space chars in column titles with underscore char
+            df.columns = df.columns.str.replace("/","-") # replace forward slash chars in column titles with dash char
+            df.columns = df.columns.str.lower() # set all column titles to lowercase
+            df.to_excel(os.path.join(new_dir,os.path.basename(file)),  index=False, header=True) # write out modified file
+
+clean_col_headers()
+
+def data_cleaning():
+    for file in os.listdir(dir):
+        f = os.path.join(dir, file)
+        if os.path.isfile(f):
+            df['average'] = df['average'].str[:-1] # remove the last character in this column. (in this case, a '%' sign)
+            df['average'] = df['average'].astype('float64') # convert this column with type 'object' to type 'float64'
+            df = df.apply(lambda x: x.str.strip() if x.dtype == 'object' else x) # remove all whitespace in all rows/cols
+            df = df.dropna(axis=1,how='all') # drop columns with all NaN values
+            df.replace('X',pd.NA,inplace=True, regex=True) # replace 'X' char with NaN
+            df.fillna(0, inplace=True) # replace all NaN values with a 0
+            # The following lines are specific to a certain set of files being worked with. Can be modified/removed as needed
+            df['col_title'] = df['col_title'].astype('float64') # convert this column with type 'object' to type 'float64'
+            df['col_title'] = df['col_title'].astype('float64') # convert this column with type 'object' to type 'float64'
+            df['col_title'] = df['col_title'].astype('float64') # convert this column with type 'object' to type 'float64'
+            df['col_title'] = df['col_title'].astype('float64') # convert this column with type 'object' to type 'float64'
+            df.to_excel(os.path.join(new_dir,os.path.basename(file)),  index=False, header=True) # write out modified file
+
+data_cleaning()
